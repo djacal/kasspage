5edfcdd821bc013103fa6ccec2ffb0b9a8189284
unknown <djacal>
2020-05-28 00:28:07 -0400

    mipage

---
+++ js/main1.js
@@ -0,0 +1,21 @@
+$(document).ready(main);
+
+var contador = 1;
+
+function main() {
+	$('.btn').click(function(){
+		if (contador == 1) {
+			$('nav').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		}
+	});
+}
+
+
---
+++ js/prueba.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+   $(".nuevo").click(function() {
+        // $('article').animate({left: "+=100px"} , 'fast');
+        $('.menu').animate({top: "+=800px"} , '.4s');
+   });
+});
+
+
+$(document).ready(function() {
+   $(".btn-1").click(function() {
+        // $('article').animate({left: "+=100px"} , 'fast');
+        $('.menu').animate({top: "0"} , '.4s');
+   });
+});
